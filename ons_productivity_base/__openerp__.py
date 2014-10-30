83b7bb49b805728419fdb91a09c29aa8f846b0e5
Sébastien Gendre <sge@open-net.ch>
2014-10-30 09:49:56 +0100

    Add the module  ons_productivity_base, at V1.0, build by Cyp. In this first version, this module only change the css on the Odo client. This change expand the with of form views.

---
+++ ons_productivity_base/__openerp__.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#
+#  File: __openerp__.py
+#  Module: ons_productivity_base
+#
+#  Created by cyp@open-net.ch
+#
+#  Copyright (c) 2014-TODAY Open-Net Ltd. <http://www.open-net.ch>
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-TODAY OpenERP S.A. <http://www.openerp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name' : 'Open-Net Productivity: base',
+    'version' : '1.0',
+    'author' : 'Open Net Sarl',
+    'category' : 'Base',
+    'description' : """
+Open Net Productivity : Base module
+-----------------------------------
+
+The 'productivity' modules is a complete family of modules offering improvement for OpenERP.
+These modules are maintained by Open Net, Swiss Partner of OpenERP.
+These modules are included in all our hosting solutions.
+
+**Features list :**
+ - CSS definition to enlarge the formulars to full screen
+
+**Author :** Open Net Sàrl   Industrie 59  1030 Bussigny  Suisse  http://www.open-net.ch
+
+**Contact :** info@open-net.ch
+
+**History :**
+
+V1.0: 2014-10-29/Cyp
+    - CSS entry to enlarge the forms
+    """,
+    'website': 'https://www.odoo.com/page/billing',
+    'images' : [],
+    'depends' : [],
+    'data': [
+        'views/onsp_base.xml',
+    ],
+    'qweb' : [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
