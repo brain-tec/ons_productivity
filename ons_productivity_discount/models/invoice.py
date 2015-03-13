d445a67bd8166069598f980a2c31b9f06fba84f3
Sébastien Gendre <sge@open-net.ch>
2015-03-13 12:25:18 +0100

    Corrected: the way taxes are computed, both in sales and invoices

--- ons_productivity_discount/models/invoice.py
+++ ons_productivity_discount/models/invoice.py
@@ -32,7 +32,7 @@ import openerp.addons.decimal_precision as dp
 class account_invoice_line(models.Model):
     _inherit = 'account.invoice.line'
 
-    # ---------- Fields management
+    # ---------- Fields management: functions
 
     @api.one
     @api.depends('price_unit', 'discount', 'invoice_line_tax_id', 'quantity',
@@ -66,6 +66,8 @@ class account_invoice_line(models.Model):
                         total = total - tax['amount']
         return total
 
+    # ---------- Fields management: definitions
+
     price_unit = fields.Float(string='Unit Price', required=True,
         digits= dp.get_precision('Product Price'),
         default=_default_price_unit)
@@ -74,11 +76,10 @@ class account_invoice_line(models.Model):
     abs_discount = fields.Float(string='Discount', digits= dp.get_precision('Discount'),
         default=0.0)
 
-
 class account_invoice(models.Model):
     _inherit = 'account.invoice'
     
-    # ---------- Fields management
+    # ---------- Fields management: functions
 
     @api.one
     @api.depends('invoice_line.price_subtotal', 'invoice_line.quantity', 'invoice_line.price_unit', 'invoice_line.discount', 'invoice_line.abs_discount')
@@ -91,5 +92,92 @@ class account_invoice(models.Model):
             else:
                 self.discount_total +=  line.quantity * (line.price_unit * ((line.discount or 0.0) / 100.0)) + line.abs_discount
     
+    @api.one
+    @api.depends('invoice_line.price_subtotal', 'tax_line.amount')
+    def _compute_amount(self):
+        self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line)
+        self.amount_tax = sum(line.amount for line in self.tax_line)
+        self.amount_total = self.amount_untaxed + self.amount_tax
+
+    # ---------- Fields management: definitions
+
     discount_total = fields.Float(string='Total of discounts', digits=dp.get_precision('Account'),
         compute='_compute_discount_total', store=True)
+
+    amount_untaxed = fields.Float(string='Subtotal', digits=dp.get_precision('Account'),
+        store=True, readonly=True, compute='_compute_amount', track_visibility='always')
+    amount_tax = fields.Float(string='Tax', digits=dp.get_precision('Account'),
+        store=True, readonly=True, compute='_compute_amount')
+    amount_total = fields.Float(string='Total', digits=dp.get_precision('Account'),
+        store=True, readonly=True, compute='_compute_amount')
+
+
+class account_invoice_tax(models.Model):
+    _inherit = 'account.invoice.tax'
+
+    # ---------- Utilities
+
+    @api.v8
+    def compute(self, invoice):
+        tax_grouped = {}
+        currency = invoice.currency_id.with_context(date=invoice.date_invoice or fields.Date.context_today(invoice))
+        company_currency = invoice.company_id.currency_id
+        for line in invoice.invoice_line:
+            taxes = line.invoice_line_tax_id.compute_all(
+                (line.quantity * (line.price_unit * (1 - (line.discount or 0.0) / 100.0))) - line.abs_discount,
+                1, line.product_id, invoice.partner_id)['taxes']
+            for tax in taxes:
+                val = {
+                    'invoice_id': invoice.id,
+                    'name': tax['name'],
+                    'amount': tax['amount'],
+                    'manual': False,
+                    'sequence': tax['sequence'],
+                    'base': currency.round(tax['price_unit'] * line['quantity']),
+                }
+                if invoice.type in ('out_invoice','in_invoice'):
+                    val['base_code_id'] = tax['base_code_id']
+                    val['tax_code_id'] = tax['tax_code_id']
+                    val['base_amount'] = currency.compute(val['base'] * tax['base_sign'], company_currency, round=False)
+                    val['tax_amount'] = currency.compute(val['amount'] * tax['tax_sign'], company_currency, round=False)
+                    val['account_id'] = tax['account_collected_id'] or line.account_id.id
+                    val['account_analytic_id'] = tax['account_analytic_collected_id']
+                else:
+                    val['base_code_id'] = tax['ref_base_code_id']
+                    val['tax_code_id'] = tax['ref_tax_code_id']
+                    val['base_amount'] = currency.compute(val['base'] * tax['ref_base_sign'], company_currency, round=False)
+                    val['tax_amount'] = currency.compute(val['amount'] * tax['ref_tax_sign'], company_currency, round=False)
+                    val['account_id'] = tax['account_paid_id'] or line.account_id.id
+                    val['account_analytic_id'] = tax['account_analytic_paid_id']
+
+                # If the taxes generate moves on the same financial account as the invoice line
+                # and no default analytic account is defined at the tax level, propagate the
+                # analytic account from the invoice line to the tax line. This is necessary
+                # in situations were (part of) the taxes cannot be reclaimed,
+                # to ensure the tax move is allocated to the proper analytic account.
+                if not val.get('account_analytic_id') and line.account_analytic_id and val['account_id'] == line.account_id.id:
+                    val['account_analytic_id'] = line.account_analytic_id.id
+
+                key = (val['tax_code_id'], val['base_code_id'], val['account_id'])
+                if not key in tax_grouped:
+                    tax_grouped[key] = val
+                else:
+                    tax_grouped[key]['base'] += val['base']
+                    tax_grouped[key]['amount'] += val['amount']
+                    tax_grouped[key]['base_amount'] += val['base_amount']
+                    tax_grouped[key]['tax_amount'] += val['tax_amount']
+
+        for t in tax_grouped.values():
+            t['base'] = currency.round(t['base'])
+            t['amount'] = currency.round(t['amount'])
+            t['base_amount'] = currency.round(t['base_amount'])
+            t['tax_amount'] = currency.round(t['tax_amount'])
+
+        return tax_grouped
+
+    @api.v7
+    def compute(self, cr, uid, invoice_id, context=None):
+        recs = self.browse(cr, uid, [], context)
+        invoice = recs.env['account.invoice'].browse(invoice_id)
+        return recs.compute(invoice)
+
