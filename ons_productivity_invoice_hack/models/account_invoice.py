99d3e4cdef137c0e9ba2fd1b4b86b3393aee4a81
Sébastien Gendre <sge@open-net.ch>
2015-03-17 22:14:58 +0100

    Lets you use "ons_productivity_invoice" while keeping the default Invoice Print button instead of the customized version.

---
+++ ons_productivity_invoice_hack/models/account_invoice.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+#  File: models/account_invoice.py
+#  Module: ons_productivity_invoice_hack
+#
+#  Created by cyp@open-net.ch
+#
+#  Copyright (c) 2015-TODAY Open-Net Ltd. <http://www.open-net.ch>
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-TODAY OpenERP S.A. <http://www.openerp.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, api, _
+from openerp.osv import fields
+from itertools import groupby
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class account_invoice(models.Model):
+    _inherit = 'account.invoice'
+    
+    @api.multi
+    def invoice_print(self):
+        """ Print the invoice and mark it as sent, so that we can see more
+            easily the next step of the workflow
+        """
+        assert len(self) == 1, 'This option should only be used for a single id at a time.'
+        self.sent = True
+        return self.env['report'].get_action(self, 'account.report_invoice')
+
