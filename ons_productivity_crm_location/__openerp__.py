ccd647eac065e3295218a98d4b072c0ca92178f5
Coninckx David <dco@open-net.ch>
2016-06-01 16:03:41 +0200

    Add new module to get city, zip, country completion into lead creation form view

---
+++ ons_productivity_crm_location/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Open Net Sarl
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Open-Net Productivity: crm base location',
+    'category': 'Open-Net customizations',
+    'author': "Open-Net Sàrl",
+    'depends': [
+        'crm',
+        'base_location'
+    ],
+    'version': '9.0.1.0.2',
+    'auto_install': False,
+    'website': 'http://open-net.ch',
+    'license': 'AGPL-3',
+    'images': [],
+    'data': [
+        'views/crm_lead_view.xml'
+    ],
+    'installable': True
+}
