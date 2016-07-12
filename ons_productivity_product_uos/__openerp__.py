af2e803cb8c46174565f7f14cb278a1e14e1b98a
Coninckx David <dco@open-net.ch>
2016-07-12 17:04:13 +0200

    Add new module to productivity

---
+++ ons_productivity_product_uos/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Coninckx David (Open Net Sarl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Open-Net Productivity: product UOS',
+    'summary': 'Improved UOS on product',
+    'category': 'Base',
+    'author': "Open Net Sàrl",
+    'depends': [
+        'product_uos',
+    ],
+    'version': '9.0.1.0.0',
+    'auto_install': False,
+    'website': 'http://open-net.ch',
+    'license': 'AGPL-3',
+    'images': [],
+    'data': [
+        'views/product_uos_view.xml'
+    ],
+    'installable': True
+}
---
+++ ons_productivity_product_uos/models/product_uos.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2016 Coninckx David (Open Net Sarl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+import openerp.addons.decimal_precision as dp
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.one
+    def _set_uos(self):
+        if self.product_id.uos_coeff:
+            self.product_uom_qty = self.product_uos_qty * self.product_id.uos_coeff
+            self.product_uom = self.product_id.uom_id
+
+    @api.one
+    def _compute_uos(self):
+        self.product_uos_qty = self.product_uom_qty / self.product_id.uos_coeff
+
+    @api.onchange('product_uos_qty')
+    def onchange_product_uos_qty(self):
+        if self.product_id:
+            self.product_uom_qty = self.product_uos_qty * self.product_id.uos_coeff
+
+    product_uos_qty = fields.Float(string='Quantity', digits=dp.get_precision('Product Unit of Measure'),
+                                    compute='_compute_uos', inverse='_set_uos', readonly=False)
+    product_uos = fields.Many2one('product.uom', string='Unit of Measure', required=True,
+                                  related='product_id.uos_id', readonly=True)
