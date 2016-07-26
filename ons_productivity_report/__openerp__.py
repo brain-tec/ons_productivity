2dfe51d800923f1eb87768ac4a43b8d12f86bb25
Coninckx David <dco@open-net.ch>
2016-07-26 16:38:09 +0200

    Add a module to reduce the font size and fix header on reports

---
+++ ons_productivity_report/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Coninckx David (Open Net Sarl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Open Net Productivity: Report',
+    'summary': 'Basic report improvoments',
+    'category': 'Open-Net Tools',
+    'author': "Open Net Sàrl",
+    'depends': [
+        'report',
+    ],
+    'version': '9.0.1.0.0',
+    'auto_install': False,
+    'website': 'http://open-net.ch',
+    'license': 'AGPL-3',
+    'images': [],
+    'data': [
+        'report.xml',
+    ],
+    'installable': True
+}
