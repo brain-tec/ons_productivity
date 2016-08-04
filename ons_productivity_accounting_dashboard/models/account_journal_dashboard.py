597321958b22a436b8cc5949a80bf1e64057e004
Coninckx David <dco@open-net.ch>
2016-08-04 15:11:35 +0200

    Conform to odoo fix

--- ons_productivity_accounting_dashboard/models/account_journal_dashboard.py
+++ ons_productivity_accounting_dashboard/models/account_journal_dashboard.py
@@ -65,10 +65,10 @@ class account_journal(models.Model):
         select_sql_clause = """SELECT sum(residual_company_signed) as total, min(date_due) as aggr_date from account_invoice where journal_id = %(journal_id)s and state = 'open'"""
         query = ''
         start_date = (first_day_of_week + timedelta(days=-7))
-        for i in range(0,6):
+        for i in range(0,7):
             if i == 0:
                 query += "("+select_sql_clause+" and date_due < '"+start_date.strftime(DF)+"')"
-            elif i == 5:
+            elif i == 6:
                 query += " UNION ALL ("+select_sql_clause+" and date_due >= '"+start_date.strftime(DF)+"')"
             else:
                 next_date = start_date + timedelta(days=7)
