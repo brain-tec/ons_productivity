bae171b91e06d97edf751c93c4be48bc05051637
Coninckx David <dco@open-net.ch>
2016-08-17 14:19:45 +0200

    Recompute bank statement balance

--- ons_productivity_accounting/models/bank_statement.py
+++ ons_productivity_accounting/models/bank_statement.py
@@ -8,3 +8,10 @@ class bank_statement(models.Model):
     _inherit = 'account.bank.statement'
 
     payment_order_lines = fields.Many2many('account.payment.line', string="Payment lines")
+
+    @api.one
+    @api.depends('line_ids', 'balance_start', 'line_ids.amount', 'balance_end_real')
+    def _end_balance(self):
+        super(bank_statement, self)._end_balance()
+        total_payment_lines = sum([line.amount_currency for line in self.payment_order_lines])
+        self.balance_end = self.balance_end - total_payment_lines
