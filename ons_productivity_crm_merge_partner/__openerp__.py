4d789ce98991e45a4c1cddc13509490ffcafb826
Open Net Sàrl <info@open-net.ch>
2016-10-17 13:31:13 +0200

    NEW : Module to allow partner merge with parent

---
+++ ons_productivity_crm_merge_partner/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 Open Net Sarl
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Open Net Productivity: CRM Partner Merge',
+    'category': 'Sales',
+    'author': "Open Net Sàrl",
+    'depends': [
+        'crm'
+    ],
+    'version': '1.0.0.0',
+    'auto_install': False,
+    'website': 'http://open-net.ch',
+    'license': 'AGPL-3',
+    'images': [],
+    'data': [
+    ],
+    'installable': True
+}
