04b035d8b8cebb78fd6a64d9bf930830e035f198
Open Net Sàrl <info@open-net.ch>
2017-01-12 09:41:52 +0100

    New module to manage initiale balance at pos session openning

---
+++ ons_productivity_pos_statements/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2016 Coninckx David (Open Net Sarl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name' : 'Open Net Productivity: POS Statements',
+    'version' : '1.0.0.0',
+    'author' : 'Open Net Sàrl',
+    'category' : 'Project',
+    'website': 'http://www.open-net.ch',
+    'images' : [],
+    'depends' : [
+        'point_of_sale'
+    ],
+    'data': [
+    ],
+    'js': [
+    ],
+    'qweb' : [
+    ],
+    'css':[
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
