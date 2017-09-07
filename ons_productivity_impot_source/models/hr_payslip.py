78a46e23500f8ec02a00b756aec2358f3d46bdf3
Odoo <odoo@cogetex-odoo-10e.open-net.ch>
2017-09-07 14:17:25 +0200

    Fix imp_src module based on cogetex comments

--- ons_productivity_impot_source/models/hr_payslip.py
+++ ons_productivity_impot_source/models/hr_payslip.py
@@ -33,6 +33,29 @@ class HrPayslip(models.Model):
             if last_payslip:
                 payslip.last_payslip_gross_amount = sum([line.amount for line in last_payslip.line_ids if line.category_id.code == 'GROSS'])
 
+    def _get_last_imp_src(self):
+        for payslip in self:
+            payslips = self.search([('employee_id', '=', payslip.employee_id.id),('id', '!=', payslip.id)])
+            last_payslip = False
+            last_payslip_imp_src = 0
+            for past_payslip in payslips:
+                if past_payslip.month <= payslip.month:
+                    last_payslip_imp_src += sum([line.amount for line in past_payslip.line_ids if line.code == 'IMP_SRC'])
+
+            payslip.last_payslip_imp_src = last_payslip_imp_src
+
+    def _get_imp_amount(self):
+        for payslip in self:
+            payslips = self.search([('employee_id', '=', payslip.employee_id.id),('id', '!=', payslip.id)])
+            last_payslip = False
+            last_payslip_imp_amount = 0
+            for past_payslip in payslips:
+                if past_payslip.month < payslip.month:
+                    last_payslip_imp_amount += sum([line.amount for line in past_payslip.line_ids if line.category_id.code == 'BASIC' or line.category_id.code == 'ALW' or line.category_id.code == 'ALW_ANU'])
+            payslip.last_payslip_imp_amount = last_payslip_imp_amount
+
     salaire_restant_annualise = fields.Float(string="Salaire Annualisé", compute="_get_annual_salary")
     last_payslip_gross_amount = fields.Float(string="Montant brut du dernier salaire", compute="_get_last_gross_payslip")
+    last_payslip_imp_src = fields.Float(string="Montant cummulé des derniers impôts à la source", compute="_get_last_imp_src")
+    last_payslip_imp_amount = fields.Float(string="Cummul imposable", compute="_get_imp_amount")
     month = fields.Integer(string="Month", compute="_get_month")
