20a5ebf17646b1060a0fdbbea3033e3769738986
Y-Phil <cyp@open-net.ch>
2017-11-14 19:45:56 +0100

    New productivity: "sale report", enhances the browsing between products and between partners by adding an date to currency rates.

---
+++ ons_productivity_sale_report/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+#  File: models/__init__.py
+#  Module: ons_productivity_sale_report
+#
+#  Created by cyp@open-net.ch
+#
+#  Copyright (c) 2017-TODAY Open-Net Ltd. All rights reserved.
+
+from . import res_currency
