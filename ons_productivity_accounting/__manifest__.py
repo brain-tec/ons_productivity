d5f306fe59113280937fb13e0c058917af628756
Open Net Sarl <jae@open-net.ch>
2018-02-02 17:32:59 +0100

    Ported ons_productivity_accounting to V11.0

---
+++ ons_productivity_accounting/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+#  File: __openerp__.py
+#  Module: ons_productivity_accounting
+#
+#  dco@open-net.ch & cyp@open-net.ch & lfr@open-net.ch
+#
+#  Copyright (c) 2016-TODAY Open-Net Ltd. All rights reserved.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name' : 'Open Net Productivity: Accounting',
+    'version' : '2.1',
+    'author' : 'Open Net Sàrl',
+    'category' : 'Accounting',
+    'website': 'http://www.open-net.ch',
+    'images' : [],
+    'depends' : [
+        'account',
+    ],
+    'data': [
+        'views/view_invoices.xml',
+    ],
+    'js': [
+    ],
+    'qweb' : [
+    ],
+    'css':[
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
