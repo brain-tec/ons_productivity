f4e76d74aba3b8ee772867e266b5ec76ea71b0b9
Open Net Sarl <jae@open-net.ch>
2018-04-24 08:34:40 +0200

    Added modules onsp bank_statement_import to import camt.054

---
+++ ons_productivity_bank_statement_import/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2018-TODAY Open-Net Ltd. All rights reserved.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name' : 'Open Net Productivity: Bank Statement Import',
+    'version' : '1.0',
+    'author' : 'Open Net Sàrl',
+    'category' : 'Accounting',
+    'website': 'http://www.open-net.ch',
+    'images' : [],
+    'depends' : [
+        'account_bank_statement_import',
+        'l10n_ch'
+    ],
+    'data': [
+        'wizard/view_import_camt54_wizard.xml'
+    ],
+    'js': [
+    ],
+    'qweb' : [
+    ],
+    'css':[
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
