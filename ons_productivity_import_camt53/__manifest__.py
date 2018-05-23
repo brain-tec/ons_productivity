7e89020a20d6d6f5e9527b9883f027462e85f79b
lfr <lfr@open-net.ch>
2018-05-23 15:10:30 +0200

    Added the fix camt53 to the productivity repo

---
+++ ons_productivity_import_camt53/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2018-TODAY Open-Net Ltd. All rights reserved.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name' : 'Open Net Productivity: Import CAMT.053',
+    'version' : '1.0',
+    'author' : 'Open Net Sàrl',
+    'category' : 'Accounting',
+    'website': 'http://www.open-net.ch',
+    'images' : [],
+    'depends' : [
+        'l10n_ch',
+        'account_bank_statement_import_camt'
+    ],
+    'data': [
+    ],
+    'js': [
+    ],
+    'qweb' : [
+    ],
+    'css':[
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
