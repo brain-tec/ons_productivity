819fc3889eadb3e1f0248c57bdbc7973925320d4
Olivier Jossen <olivier.jossen@brain-tec.ch>
2018-11-23 08:05:11 +0100

    [t112921] Finanz Konfiguration

--- ons_productivity_accounting_data/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-#  Copyright (c) 2018-TODAY Open-Net Ltd. All rights reserved.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import account_account
